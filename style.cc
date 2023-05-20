bdacb1d360ff0a07b3676d2782471b2995eb0a86
adminsitiosweb <132181410+adminsitiosweb@users.noreply.github.com>
2023-05-20 18:45:11 -0500

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,46 @@
+header{
+    background-image: url('https://www.dzoom.org.es/wp-content/uploads/2021/06/via-lactea-estrellas-nocturna.jpg');
+    padding: 15px;
+    text-align: center;
+}
+ul {
+    font-family: arial black;
+}
+h1 {
+color: white;
+font-style: italic;
+font-size: 50px;
+}
+h2{
+color: white;
+font-family: Aharoni, sans-serif;
+font-size: m;
+}
+h3{
+color: white;
+font-family: Aharoni, sans-serif;
+font-size: 25px;
+}
+main {
+padding: 10px;
+}
+body {
+    background-image: url('https://a-static.besthdwallpaper.com/earth-with-moon-from-space-wallpaper-2560x1440-55222_51.jpg');
+    color: white;
+    margin: 60px;
+    background: #000428;
+    background: -webkit-linear-gradient(to right, #004e92, #000428); 
+    background: linear-gradient(to right, #004e92, #000428);
+}
+p{
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: justify;
+}
+div{
+    padding: 10px;
+}
+h4{
+    color: white;
+    font-family: Aharoni, sans-serif;   
+    font-size: 25px;
+    }
